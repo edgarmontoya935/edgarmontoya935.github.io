555803f06a92f47d5f4cfc772319d4f4183ee9b7
Edgar Montoya <116687457+edgarmontoya935@users.noreply.github.com>
2026-05-17 01:36:15 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector(".ham");
-const navList = document.querySelector(".nav-list");
-const links = document.querySelector(".nav-list li");
-
-menu.addEventListener("click", function(){
-    this.classList.toggle("click");
-    navList.classList.toggle("open");
-})
